fb167b6f99018e4f7a04c8ee5eca01cb63ccc08a
Daniel Visicatto <dpfreestyle@gmail.com>
2023-04-26 09:13:30 -0300

    Organizando parte de Sql

--- AndreTurismo/Models/City.cs
+++ AndreTurismo/Models/City.cs
@@ -2,9 +2,15 @@
 {
     public class City
     {
-        #region[Properties]
+        #region[SqlQuerys]
         public static readonly string INSERT = "INSERT INTO [City] (Description) VALUES (@Description);" +
                                         "SELECT CAST (scope_identity() AS int)";
+
+        public static readonly string GETALL = "SELECT (Id, Description) FROM [City];"; 
+                                      
+        #endregion
+
+        #region[Properties]
         public int Id { get; set; }
         public string? Description { get; set; }
         public DateTime RegisterDate { get; set; }
