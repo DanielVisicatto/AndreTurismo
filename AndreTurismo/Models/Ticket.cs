875be76609ae5ed74bcf313ebca03a6f4ff0c6ff
Daniel Visicatto <dpfreestyle@gmail.com>
2023-04-28 15:06:26 -0300

    voltado

--- AndreTurismo/Models/Ticket.cs
+++ AndreTurismo/Models/Ticket.cs
@@ -4,19 +4,19 @@
     {
         #region[Properties]
         public int Id { get; set; }
-        public Address  Home { get; set; }
-        public Address Destiny { get; set; }
-        public Customer Customer { get; set; }
-        public DateTime Date { get; set; }
+        public Address?  Home { get; set; }
+        public Customer? Customer { get; set; }
+        public Address? Destiny { get; set; }        
+        public DateTime? Date { get; set; }
         public double Price { get; set; }
         #endregion
 
         public override string ToString()
         {
             return $"ID_Passagem:           {Id}\n" +
-                   $"Origem:                {Home}\n" +
-                   $"Destino:               {Destiny}\n" +
                    $"Cliente:               {Customer.Name}\n" +
+                   $"Origem:                {Home.City.Description}\n" +
+                   $"Destino:               {Destiny.City.Description}\n" +                  
                    $"Data:                  {Date}\n" +
                    $"Valor:                 {Price}\n\n";
         }
